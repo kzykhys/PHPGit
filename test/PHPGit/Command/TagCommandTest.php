4b248eb9366d9329cc53ed7a458795dcc5b0dde7
Kazuyuki Hayashi <hayashi@valnur.net>
2014-02-01 13:53:46 +0900

    [git-tag] Adds more tests for `git tag <tag> <commit>`

--- test/PHPGit/Command/TagCommandTest.php
+++ test/PHPGit/Command/TagCommandTest.php
@@ -40,4 +40,19 @@ class TagCommandTest extends BaseTestCase
         $git->tag->verify('v1.0.0');
     }
 
+    public function testCreateTagFromCommit()
+    {
+        $filesystem = new Filesystem();
+
+        $git = new Git();
+        $git->init($this->directory);
+        $git->setRepository($this->directory);
+        $filesystem->dumpFile($this->directory . '/README.md', 'hello');
+        $git->add('.');
+        $git->commit('Initial commit');
+        $log = $git->log(null, array('limit' => 1));
+        $git->tag->create('v1.0.0', $log[0]['hash']);
+        $this->assertCount(1, $git->tag());
+    }
+
 } 
